f026f45a967e23573476fca6689431b4aa586ce0
Szymon Polanowski <szympol@hotmail.com>
2020-03-01 23:28:15 +0100

    Segregate SFC and RF

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="app">
-    <!-- <Button :fontColor="true">Costs</Button>
-    <Button :backgroundColor="true">Suppliers</Button>
-    <Button :ovalShape="true">Clean filters</Button>
-    <Button :ovalShape="true" :boxShadow="true" :backgroundColor="true">+ Add</Button>-->
-    <ButtonFunctionalRF :fontColor="true">Costs</ButtonFunctionalRF>
-    <ButtonFunctionalRF :backgroundColor="true">Suppliers</ButtonFunctionalRF>
-    <ButtonFunctionalRF :ovalShape="true">Clean filters</ButtonFunctionalRF>
-    <ButtonFunctionalRF :ovalShape="true" :boxShadow="true" :backgroundColor="true">+ Add</ButtonFunctionalRF>
+    <div class="sf">
+      <h1>Single file functional component</h1>
+      <Button :fontColor="true">Costs</Button>
+      <Button :backgroundColor="true">Suppliers</Button>
+      <Button :ovalShape="true">Clean filters</Button>
+      <Button :ovalShape="true" :boxShadow="true" :backgroundColor="true">+ Add</Button>
+    </div>
+    <hr />
+    <div class="rf">
+      <h1>Render JSX functional component</h1>
+      <ButtonFunctionalRF :fontColor="true">Costs</ButtonFunctionalRF>
+      <ButtonFunctionalRF :backgroundColor="true">Suppliers</ButtonFunctionalRF>
+      <ButtonFunctionalRF :ovalShape="true">Clean filters</ButtonFunctionalRF>
+      <ButtonFunctionalRF :ovalShape="true" :boxShadow="true" :backgroundColor="true">+ Add</ButtonFunctionalRF>
+    </div>
   </div>
 </template>
 
 <script>
-// import Button from "./components/Button.vue";
+import Button from "./components/Button.vue";
 import ButtonFunctionalRF from "./components/ButtonFunctionalRF";
 
 export default {
   name: "App",
   components: {
-    // Button,
+    Button,
     ButtonFunctionalRF
   }
 };
